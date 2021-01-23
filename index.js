ce5b5467fe63cc98aa533a692777c2bee23653e4
Vivek Kumawat <vivekkumawat360@gmail.com>
2021-01-24 00:31:07 +0530

    added delete url button

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+require("dotenv").config();
 const express = require("express");
-const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const ShortUrl = require("./models/ShortUrl");
 const app = express();
@@ -22,23 +22,50 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
-  const shortUrls = await ShortUrl.find();
-  res.render("index", { shortUrls: shortUrls });
+  try {
+    const shortUrls = await ShortUrl.find();
+    res.render("index", { shortUrls: shortUrls });
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
 });
 
 app.get("/:shortUrl", async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ shortUrl: req.params.shortUrl });
-  if (shortUrl == null) return res.sendStatus(400);
-  shortUrl.clicks++;
-  shortUrl.save();
-  res.redirect(shortUrl.fullUrl);
+  try {
+    const shortUrl = await ShortUrl.findOne({ shortUrl: req.params.shortUrl });
+    if (shortUrl == null) return res.sendStatus(400);
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.fullUrl);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+});
+
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const response = await ShortUrl.findByIdAndDelete(id);
+    if (response == null) return res.sendStatus(400);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
 });
 
 app.post("/shortUrl", async (req, res) => {
-  await ShortUrl.create({
-    fullUrl: req.body.fullUrl,
-  });
-  res.redirect("/");
+  try {
+    await ShortUrl.create({
+      fullUrl: req.body.fullUrl,
+    });
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
